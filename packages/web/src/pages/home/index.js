1228c3ddad02aa53c7765a2c6ffc02ba8e39c133
Theodoros Itzaris <theo.itzaris@circusstreet.com>
2019-12-04 17:54:29 +0000

    Update Home page

--- packages/web/src/pages/home/index.js
+++ packages/web/src/pages/home/index.js
@@ -1,7 +1,18 @@
 import React, { useEffect, useState } from 'react'
-import { List } from '@pr/components'
+import { List, Panel } from '@pr/components'
 import { fetchData } from '@pr/service-api'
-//import { Player } from 'video-react';
+import {
+    BigPlayButton,
+    ClosedCaptionButton,
+    Player,
+    ControlBar,
+    ReplayControl,
+    ForwardControl,
+    CurrentTimeDisplay,
+    TimeDivider,
+    PlaybackRateMenuButton,
+    VolumeMenuButton
+  } from 'video-react';
 
 import './index.css'
 
@@ -15,29 +26,61 @@ const defaultData = [{
     }]
   }]
 
+  const divStyle={
+      width: '100%'
+    }
+
 export default () => {
     const [chapters, setChapters] = useState(defaultData);
+    const [sceneItemSelected, setSceneItemSelected] = useState();
+    const [activeChapter, setActiveChapter] = useState({})
+
+    // update the selected scene
+    // update the active chapter
+    function handleClick(values){
+        setSceneItemSelected(values.sceneId)
+        setActiveChapter({
+            ChapterName: values.ChapterName,
+            ChapterDescr: values.ChapterDescr
+        })
+    }
 
     useEffect(() => {
         fetchData()
             .then(data =>
             {	
                 setChapters({data: JSON.parse(data).chapters});
-                console.log('****', JSON.parse(data).chapters)
             }
             )
             .catch(error => console.log('error:', error))
     },[])
 
-    console.log('1st:', chapters)
     return(
         <div className="main">
-            <div className="col-1">
-                {!chapters && <div>Loading...</div>}
-                {chapters && <List data={chapters.data || chapters} />}
+            <div className="row">
+                <div className="col-1">
+                    {!chapters && <div>Loading...</div>}
+                    {chapters && <List data={chapters.data || chapters} handleClick={handleClick} sceneItemSelected={sceneItemSelected}/>}
+                </div>
+                <div className="col-2">
+                    <Player autoPlay={false} src="http://media.circusstreet.com/mobile-marketing-part-1-v3/video/S1_1-171025.mp4">
+                        <BigPlayButton position="center" />
+                        <ControlBar>
+                            <ReplayControl seconds={10} order={1.1} />
+                            <ForwardControl seconds={30} order={1.2} />
+                            <CurrentTimeDisplay order={4.1} />
+                            <TimeDivider order={4.2} />
+                            <PlaybackRateMenuButton rates={[5, 2, 1, 0.5, 0.1]} order={7.1} />
+                            <VolumeMenuButton enabled />
+                            <ClosedCaptionButton order={7} />
+                        </ControlBar>
+                    </Player>
+                </div>
             </div>
-            <div className="col-2">
-        column 2nd
+            <div className="row">
+                <div style={divStyle}>
+                    <Panel title={activeChapter.ChapterName} text={activeChapter.ChapterDescr} />
+                </div>
             </div>
         </div>
     )
