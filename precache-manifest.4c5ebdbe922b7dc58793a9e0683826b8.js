9c1f05be3837b28589a575cd82ff03d892335a16
Theodoros Itzaris <theo.itzaris@circusstreet.com>
2019-12-04 21:47:33 +0000

    Updates

---
+++ precache-manifest.4c5ebdbe922b7dc58793a9e0683826b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12a85f4df269b143fc9ae76ad8ed88cd",
+    "url": "/react-monorepo/index.html"
+  },
+  {
+    "revision": "624d8f95d59d2a772f24",
+    "url": "/react-monorepo/static/css/main.36d20833.chunk.css"
+  },
+  {
+    "revision": "c89e1e4fe6eae3c4c5c4",
+    "url": "/react-monorepo/static/js/2.fccdd8e3.chunk.js"
+  },
+  {
+    "revision": "624d8f95d59d2a772f24",
+    "url": "/react-monorepo/static/js/main.e5f664c5.chunk.js"
+  },
+  {
+    "revision": "1507c2ab0e61e7b0d389",
+    "url": "/react-monorepo/static/js/runtime~main.7fbd862f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6045c30af11a15c5244a461896382196.js"
+  "/react-monorepo/precache-manifest.4c5ebdbe922b7dc58793a9e0683826b8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-monorepo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
